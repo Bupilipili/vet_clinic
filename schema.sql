2288c532227acca6a991085fed7b58d6428e2162
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-09-21 18:25:27 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -29,3 +29,29 @@ ALTER TABLE animals
     DROP COLUMN IF EXISTS species,
     ADD COLUMN species_id INTEGER REFERENCES species(id),
     ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
+-- Create the vets table
+ CREATE TABLE vets (
+    id serial PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    date_of_graduation DATE
+);
+
+-- Create the specializations table
+CREATE TABLE specializations (
+    vet_id INTEGER REFERENCES vets(id),
+    species_id INTEGER REFERENCES species(id),
+    PRIMARY KEY (vet_id, species_id)
+);
+
+   
+-- Create the visits table
+CREATE TABLE visits (
+    animal_id INT,
+    vet_id INT,
+    visit_date DATE,
+    FOREIGN KEY (animal_id) REFERENCES animals(id),
+    FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
+
